2bf9ac0e2cc8c83e6d621b30567ed001c64f06a4
tatekitzhak <tatekosi@gmail.com>
2022-07-13 22:45:23 +0300

    with error

--- src/app.js
+++ src/app.js
@@ -1,14 +1,61 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { Box, Typography } from "@material-ui/core";
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import MenuRounded from '@material-ui/icons/MenuRounded';
+import {
+   Root,
+   Header,
+   Nav,
+   Content,
+   Footer,
+   presets,
+} from 'mui-layout';
 
-class App extends Component{
-   render(){
-      return(
-         <div>
-            <h1>Hello World material-ui</h1>
-            <Button variant="contained">Hello World</Button>
-         </div>
-      );
-   }
-}
+const baseTheme = createMuiTheme(); // or use your own theme;
+const config = presets.createStandardLayout();
+
+const App = () => (
+   <ThemeProvider theme={baseTheme}>
+      <Root config={config}>
+         <Header
+            renderMenuIcon={open => (open ? <ChevronLeft /> : <MenuRounded />)}
+         >
+            header
+       </Header>
+         <Nav
+            renderIcon={collapsed =>
+               collapsed ? <ChevronRight /> : <ChevronLeft />
+            }
+         >
+            nav
+       </Nav>
+         <Content>
+            <Box p={{ xs: 2, sm: 3 }}>
+               <Typography variant="h6" gutterBottom>
+                  <b>This is content!</b>
+               </Typography>
+               <Typography color="textSecondary">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam
+                  aliquam sem et tortor consequat id porta. Adipiscing commodo elit
+                  at imperdiet. Lacus sed turpis tincidunt id aliquet risus feugiat
+                  in ante. Cras tincidunt lobortis feugiat vivamus at augue eget
+                  arcu. Hendrerit dolor magna eget est. Fames ac turpis egestas
+                  maecenas. Cras semper auctor neque vitae tempus quam pellentesque.
+                  Amet nisl purus in mollis nunc sed id semper. Molestie ac feugiat
+                  sed lectus vestibulum mattis ullamcorper velit sed. Id aliquet
+                  lectus proin nibh nisl condimentum.
+            </Typography>
+            </Box>
+         </Content>
+         <Footer>
+            footer
+       </Footer>
+      </Root>
+   </ThemeProvider>
+)
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-<<<<<<< HEAD
 import { createStore, combineReducers} from 'redux';
 import { Provider} from 'react-redux';
-
-
-
-import {store} from './redux/stores/store.js';
-import App1 from './apps/app1.js';
-
-
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
-    <Provider store={store}>
-        <App1 rooProps="App home" />
-    </Provider>
-);
-=======
-
 import App from './app.js';
-console.log('index:')
->>>>>>> dev
-
 
 const container = document.getElementById('root');
 const root = createRoot(container);
